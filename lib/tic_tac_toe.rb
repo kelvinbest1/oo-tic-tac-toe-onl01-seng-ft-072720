7db8444f7037ee4feb8dba655dd0f1af4f03e606
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-08 02:04:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,33 +10,4 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
-def position_taken?(array, index)
-  if (array[index] == " " || array[index] == "" || array[index] == nil)
-    return false
-  else
-    return true
-  end
-endve?(array, index)
-  if (index
-def valid_mo
-endter 1-9:"
-  input = )
-  if (tu]
-   
    
